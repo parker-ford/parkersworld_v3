2b78733d8a4ea39998e3de5cadf2a4c599239e2e
parker-ford <35358924+parker-ford@users.noreply.github.com>
2024-01-30 10:34:17 -0800

    plane normals

--- src/lib/ParkersRenderer/Meshes/PlaneMesh.js
+++ src/lib/ParkersRenderer/Meshes/PlaneMesh.js
@@ -19,12 +19,14 @@ export class PlaneMesh extends Mesh {
     calculateVertexCoordinates(){
         this.vertexCoordinates = [];
         this.uvCoordinates = [];
+        this.normalCoordinates = [];
         const widthInterval = 1 / this.width;
         const heightInterval = 1 / this.height;
         for(let i = 0; i < this.width + 1; i++){
             for(let j = 0; j < this.height + 1; j++){
                 this.vertexCoordinates.push([ -0.5 + i * widthInterval, -0.5 + j * heightInterval, 0, 1]);
                 this.uvCoordinates.push([i * widthInterval, j * heightInterval]);
+                this.normalCoordinates.push([0, 0, 1]);
             }
         }
     }
@@ -32,6 +34,7 @@ export class PlaneMesh extends Mesh {
     calculateTriangleVertices(){
         this.triangleCoordinates = [];
         this.uvs = [];
+        this.normals = [];
 
         for(let i = 0; i < this.width; i++){
             for(let j = 0; j < this.height; j++){
@@ -45,6 +48,11 @@ export class PlaneMesh extends Mesh {
                 this.uvs.push(this.uvCoordinates[(j + 1) + (i * (this.height + 1))]);
                 this.uvs.push(this.uvCoordinates[(j + 1) + ((i + 1) * (this.height + 1))]);
 
+                this.normals.push(this.normalCoordinates[j + (i * (this.height + 1))]);
+                this.normals.push(this.normalCoordinates[(j + 1) + (i * (this.height + 1))]);
+                this.normals.push(this.normalCoordinates[(j + 1) + ((i + 1) * (this.height + 1))]);
+
+
                 //Bottom Triangle
                 this.triangleCoordinates.push(this.vertexCoordinates[(j + 1) + ((i + 1) * (this.height + 1))]);
                 this.triangleCoordinates.push(this.vertexCoordinates[j + ((i + 1) * (this.height + 1))]);
@@ -54,6 +62,10 @@ export class PlaneMesh extends Mesh {
                 this.uvs.push(this.uvCoordinates[j + ((i + 1) * (this.height + 1))]);
                 this.uvs.push(this.uvCoordinates[j + (i * (this.height + 1))]);
 
+                this.normals.push(this.normalCoordinates[(j + 1) + ((i + 1) * (this.height + 1))]);
+                this.normals.push(this.normalCoordinates[j + ((i + 1) * (this.height + 1))]);
+                this.normals.push(this.normalCoordinates[j + (i * (this.height + 1))]);
+
             }
         }
 
@@ -62,11 +74,9 @@ export class PlaneMesh extends Mesh {
         this.triangleColors = new Float32Array(
             Array(this.triangleVertices.length).fill(1.0)
         );
-        
+
         this.triangleUVs = new Float32Array(this.uvs.flat());
-        this.triangleNormals = new Float32Array(
-            Array(this.triangleCoordinates.length * 3).fill(1.0)
-        );
+        this.triangleNormals = new Float32Array(this.normals.flat());
 
     }
 
