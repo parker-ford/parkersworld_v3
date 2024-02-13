21fd8b7d38403a5b93d73f64fdb6079fa7448fbc
parker-ford <35358924+parker-ford@users.noreply.github.com>
2024-02-12 16:54:10 -0800

    wireframe fix

--- src/lib/ParkersRenderer/Meshes/SphereMesh.js
+++ src/lib/ParkersRenderer/Meshes/SphereMesh.js
@@ -91,18 +91,39 @@ export class SphereMesh extends Mesh {
 
     calculateLineVertices(){
         let lines = [];
+        let line_uvs = [];
+        let line_normals = [];
+
         for(let i = 0; i < this.triangleCoordinates.length; i+=3){
             //Line 1
             lines.push(this.triangleCoordinates[i]);
             lines.push(this.triangleCoordinates[i+1]);
 
+            line_uvs.push(this.uvs[i]);
+            line_uvs.push(this.uvs[i+1]);
+
+            line_normals.push(this.normals[i]);
+            line_normals.push(this.normals[i+1]);
+
             //Line 2
             lines.push(this.triangleCoordinates[i + 1]);
             lines.push(this.triangleCoordinates[i + 2]);
 
+            line_uvs.push(this.uvs[i + 1]);
+            line_uvs.push(this.uvs[i + 2]);
+
+            line_normals.push(this.normals[i + 1]);
+            line_normals.push(this.normals[i + 2]);
+
             //Line 3
             lines.push(this.triangleCoordinates[i + 2]);
             lines.push(this.triangleCoordinates[i]);
+
+            line_uvs.push(this.uvs[i + 2]);
+            line_uvs.push(this.uvs[i]);
+
+            line_normals.push(this.normals[i + 2]);
+            line_normals.push(this.normals[i]);
         }
 
         this.lineVertices = new Float32Array(lines.flat());
@@ -110,11 +131,7 @@ export class SphereMesh extends Mesh {
         this.lineColors = new Float32Array(
             Array(this.lineVertices.length).fill(1.0)
         );
-        this.lineUVs = new Float32Array(
-            Array(lines.length * 2).fill(1.0)
-        );
-        this.lineNormals = new Float32Array(
-            Array(lines.length * 3).fill(1.0)
-        );
+        this.lineUVs = new Float32Array(line_uvs.flat());
+        this.lineNormals = new Float32Array(line_normals.flat());
     }
 }
