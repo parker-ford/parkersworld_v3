2919030b4e36bf67f829ad19f7e95294e126f8ac
parker-ford <35358924+parker-ford@users.noreply.github.com>
2024-02-13 17:46:01 -0800

    obj loaded

---
+++ src/routes/posts/08/scene.js
@@ -0,0 +1,46 @@
+import * as PW from '$lib/ParkersRenderer'
+export const createScene = async (el, onLoaded) => {
+    onLoaded();
+
+    el.width = Math.min(document.body.clientWidth, 1400);
+    el.height = Math.min(document.body.clientWidth, 1400) * .5;
+
+    const renderer = new PW.Renderer(el);
+    if (! await renderer.init()) {
+        console.log("renderer initialization failed");
+    }
+
+    const scene = new PW.Scene();
+
+    const camera = new PW.PerspectiveCamera({
+        fov: 45,
+        aspect: el.width / el.height,
+        near: 0.1,
+        far: 100,
+    });
+
+    scene.add(camera);
+    camera.transform.position[2] = -5;
+
+    const objMesh = new PW.OBJMesh({
+        filePath: "../models/Cube/Cube.obj"
+    });
+    await objMesh.loaded();
+
+    // const objMesh = new PW.CubeMesh({
+
+    // });
+
+    const obj = new PW.Renderable({
+        mesh: objMesh,
+        material: new PW.NormalMaterial({color: [1, 0, 0, 1]})
+    });
+    scene.add(obj)
+
+    function frame() {
+
+        renderer.render(scene, camera);
+        requestAnimationFrame(frame);
+    }
+    frame();
+}
