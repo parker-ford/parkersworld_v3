37bfd974c98c92fd96f065382b8de95585b3a78e
parker-ford <35358924+parker-ford@users.noreply.github.com>
2024-02-22 18:09:11 -0800

    color bug fix in 09

--- src/routes/posts/09/scene.js
+++ src/routes/posts/09/scene.js
@@ -16,6 +16,17 @@ export const createScene = async (el, onLoaded) => {
         color: 0xff0000,
     }
 
+    const alignGUIWithCanvas = () => {
+        const canvasRect = el.getBoundingClientRect();
+        const guiRect = gui.domElement.getBoundingClientRect();
+        gui.domElement.style.position = 'absolute';
+        gui.domElement.style.top = `222px`;
+        gui.domElement.style.left = `${canvasRect.right - guiRect.width - 2}px`;
+        console.log("testing gui align");
+        console.log(guiRect)
+    };
+    alignGUIWithCanvas();
+
     const renderer = new PW.Renderer(el);
     if (! await renderer.init()) {
         console.log("renderer initialization failed");
@@ -33,35 +44,43 @@ export const createScene = async (el, onLoaded) => {
     scene.add(camera);
     camera.transform.position[2] = -9;
 
-    const dragonMesh = new PW.OBJMesh({
-        filePath: "../models/Dragon/DragonShadeSmooth.obj",
+    // const dragonMesh = new PW.OBJMesh({
+    //     filePath: "../models/Dragon/DragonShadeSmooth.obj",
+    //     wireframe: false
+    // });
+    // const dragonTransform = new PW.Transform();
+    // dragonTransform.scale = [10,10,10];
+    // dragonTransform.position[1] = -1;
+    // quat.rotateX(dragonTransform.rotation, dragonTransform.rotation, Math.PI / 2);
+    // await dragonMesh.loaded();
+
+    const bunnyMesh = new PW.OBJMesh({
+        filePath: "../models/StanfordBunny/StanfordBunnyShadeSmooth.obj",
         wireframe: false
     });
-    const dragonTransform = new PW.Transform();
-    dragonTransform.scale = [10,10,10];
-    dragonTransform.position[1] = -1;
-    quat.rotateX(dragonTransform.rotation, dragonTransform.rotation, Math.PI / 2);
+    const bunnyTransform = new PW.Transform();
+    bunnyTransform.position = [0,-1,0];
+    bunnyTransform.scale = [8,8,8];
+    quat.rotateY(bunnyTransform.rotation, bunnyTransform.rotation, Math.PI );
+    await bunnyMesh.loaded();
 
-    await dragonMesh.loaded();
     onLoaded();
     
-    const sphere = new PW.Renderable({
-        mesh: dragonMesh,
-        material: new PW.GoochMaterial({
-            color: [1, 0, 0],
-            wireframe: false
-        }),
-    })
-    scene.add(sphere);
-    sphere.transform = dragonTransform;
+    const obj = new PW.Renderable({
+        mesh: bunnyMesh,
+        material: new PW.BasicMaterial({color: [1, 0, 0, 1]})
+    });
+    obj.transform = bunnyTransform;
+    scene.add(obj);
+
 
     gui.addColor(parameters, 'color').onChange((value) => {
         let r = (value & 0xff0000) >> 16;
         let g = (value & 0x00ff00) >> 8;
         let b = (value & 0x0000ff);
-        let color = [r/255, g/255, b/255];
-        sphere.material.color = color;
-        sphere.material.updateMaterialBuffers();
+        let color = [r/255, g/255, b/255, 1];
+        obj.material.color = color;
+        obj.material.updateMaterialBuffers();
 
     })
 
