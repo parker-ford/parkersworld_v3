23fbf3fc18bed64401e43277d95fc631d922643c
parker-ford <35358924+parker-ford@users.noreply.github.com>
2024-06-27 18:08:12 -0700

    empty preemo ray default

---
+++ src/routes/default_empty/PRAY_empty/scene.js
@@ -0,0 +1,100 @@
+import * as PRAY from '$lib/Preemo-Ray'
+import { quat, vec3 } from 'gl-matrix';
+import GUI from 'lil-gui'; 
+
+
+export const createScene = async (el, onLoaded) => {
+
+    const fallbackVideo = document.getElementById('fallback-video');
+    
+    //Initial setup
+    el.width = Math.min(document.body.clientWidth, 1400);
+    el.height = Math.min(document.body.clientWidth, 1400) * .5;
+
+    // GUI
+    const gui = new GUI()
+    gui.domElement.id = 'gui';
+
+
+    //Renderer
+    const renderer = new PRAY.Renderer(el);
+    if (! await renderer.init()) {
+        console.log("renderer initialization failed");
+        fallbackVideo.style.display = 'block';
+        fallbackVideo.width = el.width;
+        fallbackVideo.height = el.height;
+        el.style.display = 'none';
+        gui.domElement.style.display = 'none';
+        
+        onLoaded();
+        const modal = document.getElementById('webgpu__modal');
+        if (localStorage.getItem("hideWebGPUModal") !== "true") {
+            modal.showModal();
+        }
+
+        return;
+    }
+
+    const scene = new PRAY.Scene();
+    onLoaded();
+
+    //Camera
+    const camera = new PRAY.PerspectiveCamera({
+        fov: 90,
+        aspect: el.width / el.height,
+        near: 0.01,
+        far: 10000,
+        image_size: [el.width, el.height],
+        lens_focal_length: 0.035,
+        image_plane_distance: 0.05,
+        fstop: 2.8,
+        focal_length: 0.035,
+    });
+    camera.transform.position = [0, 0, -3];
+    scene.add(camera);
+
+    //Materials
+    const default_material = new PRAY.Material({
+        attenuation: vec3.fromValues(0.8, 0.5, 0.5),
+        material_flag: PRAY.Material.TYPES.LAMBERTIAN,
+    });
+    scene.add(default_material);
+
+
+    //Spheres
+    const default_sphere = new PRAY.Sphere({
+        position: [0.0, 0.0, 0.0],
+        radius: 1,
+        material_id: default_material.id,
+    });
+    scene.add(default_sphere);
+
+
+    //Frame
+    function frame() {
+        renderer.render(scene, camera);
+        requestAnimationFrame(frame);
+    }
+    frame();
+
+    const parameters= {
+        lens_focal_length: camera.lens_focal_length,
+        image_plane_distance: camera.image_plane_distance,
+        fstop: camera.fstop,
+        focal_length: camera.focal_length,
+    }
+
+    const alignGUIWithCanvas = () => {
+        const canvasRect = el.getBoundingClientRect();
+        const guiRect = gui.domElement.getBoundingClientRect();
+        gui.domElement.style.position = 'absolute';
+        gui.domElement.style.top = `222px`;
+        gui.domElement.style.left = `${canvasRect.right - guiRect.width - 2}px`;
+        camera.setGui(gui);
+    };
+    alignGUIWithCanvas();
+
+
+
+
+}
---
+++ src/routes/posts/12/+page.svelte
@@ -0,0 +1,63 @@
+<script context="module">
+    export const metadata = {
+        title : "placeholder title",
+        date : "01/01/0001",
+        image_static: 'images/thumbnails/placeholder/placeholder.jpg',
+		image: 'images/thumbnails/placeholder/placeholder.jpg',
+        logo: 'images/logos/webGPULogo.png',
+    }
+</script>
+
+<script>
+	import { onMount } from 'svelte';
+    import '../../style.css'
+
+    let el;
+    import { sceneLoaded } from '../../stores.js';
+    onMount(async () =>{
+        const {createScene} = await import('./scene')
+        createScene(el, () => sceneLoaded.set(true))
+    })
+
+</script>
+
+<div class="page__main">
+    <div class = "page__content">
+        <div class="page__canvas__container">
+            <canvas class="page__canvas" bind:this={el}></canvas>
+            <video id="fallback-video" width="640" height="360" controls style="display: none;">
+                <source src="../videos/rayTracingBeginning.mp4" type="video/mp4">
+                <track kind="captions">
+                Your browser does not support the video tag.
+            </video>
+        </div>
+        <div class ="page__title__container">
+            <h1 class="page__title">{metadata.title}</h1>
+            <h5>{metadata.date}</h5>
+        </div>
+        <div class="page__text__container">
+            <h4 class="page__sub-subtitle">Controls</h4>
+            <img class="controls__image" src="../images/icons/controls.png" alt="Transformations" />
+
+            <h4 class="page__sub-subtitle">Overview</h4>
+            <p class="page__body">
+                This is the first post in a series I am making on my process of creating a ray tracer using the WebGPU API. The demo above is interactive, so try messing around with it. I still plan on continuing to work on my other rendering engine, but working on a ray tracer was calling my name so I will probably be bouncing between both projects. This post follows the book <a href="https://www.realtimerendering.com/">Ray Tracing in One Weekend</a> by Peter Shirley, Trevor David Black, and Steve Hollasch. It is a really good introduction to ray tracing, so make sure to check that out. There are a couple of sequel books as well, and I will be looking to those as I continue to work on this project.
+                <br><br>
+                If you want to check out the code, you can find it <a href ="https://github.com/parker-ford/Preemo-Ray">here</a>.
+            </p>
+
+            <h4 class="page__sub-subtitle">TITLE</h4>
+            <p class="page__body">
+                <img class="page__img" src="../images/posts/rayTracingBeginning/raytraced.jpg" alt="raytraced iamge" width="50%"/>
+                <span class="page__img__sub-text">EXAMPLE TEXT</span>
+
+
+            </p>
+
+            <h4 class="page__sub-subtitle">Resources</h4>
+
+            <p class="page__body">
+            </p>  
+        </div>
+    </div>
+</div>
